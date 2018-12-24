40cd9954dd37b7c7416877b05bd894b99f20b9d0
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2018-12-24 11:37:35 -0500

    Neural newtork implemented with action selection policy

--- ai_new.py
+++ ai_new.py
@@ -13,7 +13,7 @@ import torch.nn as nn
 import torch.nn.functional as func
 import torch.optim as optim  # optimizer for stochastic gradient descent
 import torch.autograd as autograd
-from torch.autograd import variable
+from torch.autograd import Variable
 from typing import Deque
 
 # Creating Neural Network Architecture
@@ -25,8 +25,8 @@ class Network(nn.Module):
     def __init__(self, input_size, nb_action):
         """Initializes the object of type Network."""
         super(Network, self).__init__()
-        self.input_size = input_size
-        self.nb_action = nb_action
+        self.input_size = input_size  # the number of dimensions in the vectors that are encoding the states
+        self.nb_action = nb_action  # number of possible actions that the agent can make
         self.fc1 = nn.Linear(input_size, 30)
         self.fc2 = nn.Linear(30, nb_action)
         # the last two lines define how many neurons in each layer. in current setup, input_size in input layer,
@@ -44,7 +44,7 @@ class Network(nn.Module):
 class Replay(object):
     """A class handling experience replay for the model."""
 
-    def __init__(self, capacity=100):
+    def __init__(self, capacity=100000):
         self.capacity = capacity  # the number of past experiences you want to store.
         self.memory = Deque(maxlen=capacity)  # Deque is a collection type that will store stuff up till a max length
         # and pop everything thereafter
@@ -56,6 +56,31 @@ class Replay(object):
         # convert it to form sublists of all indices i of both lists together.
         # if there are more things in one sublist than are in the other sublist, the elements with no counterpart
         # in the shorter sublist are ignored by the function
-        return map(lambda x: variable(torch.cat(x, 0)), samples)
+        return map(lambda x: Variable(torch.cat(x, 0)), samples)
+
+
+# Implementing Deep-Q Learning
+
+class DeepQNetwork:
+    """A class representing the Deep Q-Learning Network for the self driving car."""
+
+    def __init__(self, input_size, nb_action, gamma):
+        self.gamma = gamma
+        self.reward_window = []
+        self.model = Network(input_size, nb_action)
+        self.memory = Replay()  # we took defualt = 100,000
+        self.optimizer = optim.Adam(self.model.parameters(), lr=0.001)
+        # optimizer performing stochastic gradient descent
+        self.last_state = torch.Tensor(input_size).unsqueeze(0)
+        self.last_action = 0
+        self.last_reward = 0
+
+    def select_action(self, state):
+        """Selects the optimal action for the agent to take."""
+        probabilities = func.softmax(self.model.forward(Variable(state, volatile=True))*7)  # Temperature parameter = 7
+        # Temperature parameters change the probabilities by polarizing them
+        # Higher temp param => higher probs get higher and lower probs get lower
+        action = probabilities.multinomial()
+        return action.data[0, 0]
 
     
